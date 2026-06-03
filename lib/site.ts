634ed3a5d80c6162cb4e96b3321cc6db2e39bae2
mconklin-boop <mconklin@conklininvestments.com>
2026-06-03 17:10:17 -0600

    Add Jotform prayer request setting

--- lib/site.ts
+++ lib/site.ts
@@ -1,6 +1,9 @@
 // TODO: Replace with approved live donation/payment link.
 export const DONATION_URL = "#";
 
+// TODO: Replace with the live Jotform prayer request form URL.
+export const JOTFORM_PRAYER_REQUEST_URL = "";
+
 export const contact = {
   phone: "(720) 258-6272",
   email: "desiree@foundationontherock.org",
